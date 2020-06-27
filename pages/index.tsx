0af6cd04229de7f83c9ec50df95fd0188ae73f1d
dbetteridge <danielrbetteridge@gmail.com>
2020-06-27 20:53:13 +0100

    Fixing homepage layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const index = () => {
     <StyledCard
       style={{ margin: "2rem", minHeight: "calc(100vh - 50px - 4rem)" }}
     >
-      <Row style={{ width: "100%", maxHeight: "calc(100vh - 50px - 4rem)" }}>
+      <Row style={{ width: "100%" }}>
         <Col md={6} xs={2}></Col>
         <Col md={12} xs={20}>
           <Title level={2}>A documentation sharing space</Title>
@@ -24,25 +24,27 @@ const index = () => {
           </Paragraph>
           <Paragraph>Access is given securely using OAuth</Paragraph>
           <Title level={3}>Features</Title>
-          <ul>
-            <li>
-              You can share your document spaces with anyone regardless of their
-              access to your git repositories
-            </li>
-            <li>
-              The markdown files are downloaded, converted to HTML and stored in
-              an access controlled database
-            </li>
-            <li>
-              Styling is applied at load to make them readable on mobile, tablet
-              and desktop
-            </li>
-            <li>Automatically generated table of contents per repository</li>
-            <li>
-              Clicking through from your repository will show the table of
-              contents and you can select a file to view
-            </li>
-          </ul>
+          <Paragraph>
+            <ul>
+              <li>
+                You can share your document spaces with anyone regardless of
+                their access to your git repositories
+              </li>
+              <li>
+                The markdown files are downloaded, converted to HTML and stored
+                in an access controlled database
+              </li>
+              <li>
+                Styling is applied at load to make them readable on mobile,
+                tablet and desktop
+              </li>
+              <li>Automatically generated table of contents per repository</li>
+              <li>
+                Clicking through from your repository will show the table of
+                contents and you can select a file to view
+              </li>
+            </ul>
+          </Paragraph>
           <br />
           <div style={{ textAlign: "center" }}>
             <Link href="/register">
@@ -64,7 +66,7 @@ const index = () => {
       </Row>
 
       <Row style={{ width: "100%", justifyContent: "center" }}>
-        <Col md={8} xs={20}>
+        <Col md={20} xs={20}>
           <img
             src={"/Repositories.png"}
             width={"100%"}
@@ -72,10 +74,10 @@ const index = () => {
           />
         </Col>
 
-        <Col md={8} xs={20}>
+        <Col md={20} xs={20}>
           <img src={"/TOC.png"} width={"100%"} style={{ maxWidth: "800px" }} />
         </Col>
-        <Col md={8} xs={20}>
+        <Col md={20} xs={20}>
           <img
             src={"/Document.png"}
             width={"100%"}
